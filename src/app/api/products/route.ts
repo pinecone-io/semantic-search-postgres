40ecec8c7cde90f44b5ec2e1c9f6854b14b1e116
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2023-10-05 16:42:07 -0700

    init

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,53 @@
+
+// pages/api/products.ts
+import { NextRequest, NextResponse } from 'next/server';
+import { Pinecone } from '@pinecone-database/pinecone'
+import { getEmbeddings } from '@/utils/embeddings';
+import db from '@/utils/db';
+
+
+type Metadata = {
+  id: string
+  description: string
+}
+
+const pinecone = new Pinecone()
+const limit = 10
+
+
+async function handler(req: NextRequest) {
+  const { searchTerm, currentPage } = await req.json();
+  console.log(searchTerm, currentPage)
+  const offset = currentPage > 1 ? (currentPage - 1) * limit : currentPage
+
+  const pgClient = await db.getClient()
+
+  const index = await pinecone.index<Metadata>(process.env.PINECONE_INDEX!)
+
+
+  const embeddedSearchTerm = await getEmbeddings(searchTerm)
+
+  const result = await index.query({
+    vector: embeddedSearchTerm,
+    topK: 100,
+    includeMetadata: true
+  })
+
+  console.log(result)
+
+  const ids = result.matches?.map((match) => match.metadata?.id)
+
+  const products = await pgClient.query(`
+    SELECT * FROM products 
+    WHERE id IN (${ids?.map((id) => `'${id}'`).join(',')})
+    LIMIT ${limit} OFFSET ${offset}    
+  `)
+
+  return NextResponse.json(products.rows)
+
+}
+
+
+export {
+  handler as POST
+}
